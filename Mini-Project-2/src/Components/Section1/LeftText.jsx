57053139649888de8879e4da3997e057aa33c658
Navneet Yadav <navneet.y7027@gmail.com>
2026-04-04 07:31:56 +0530

    2nd-miniProject-stage0

---
+++ Mini-Project-2/src/Components/Section1/LeftText.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftText = () => {
+  return <div>LeftText</div>;
+};
+
+export default LeftText;
---
+++ Mini-Project-2/src/Components/Section1/Section1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Section1 = () => {
+  return <div className="h-screen w-full bg-gray-900 text-white">Section1</div>;
+};
+
+export default Section1;
